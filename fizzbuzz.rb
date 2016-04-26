a0e1f284055dd0bd9e4bd9971dbcec881dfa0aea
Monroe Lee <mlee.js@gmail.com>
2016-04-26 20:04:55 +0000

    first

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,11 @@
-1.upto(100) do |i|
+ puts "enter starting number"
+ a = gets.chomp.to_i
+ puts "starting number is #{a}" 
+ puts "enter ending number"
+ b = gets.chomp.to_i
+ puts "ending number is #{}"
+
+a.upto(b) do |i|
   if i % 5 == 0 && i % 3 == 0
     puts "FizzBuzz"
   elsif i % 5 == 0
